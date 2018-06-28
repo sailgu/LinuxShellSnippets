2f34d6318746a127b6ff7f1ed5c8549f5204e5c2
sailgu <juyou3hui@gmail.com>
2018-06-28 23:46:10 +0800

    Create vimThemeSetting.sh

---
+++ Linux/vimThemeSetting.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+mkdir ~/.vim
+mkdir ~/.vim/colors/
+mv solarized.vim  ~/.vim/colors/
+
+## setting in .vimrc
+# set background=dark
+# colorscheme solarized
