7cf418d345828b3370d3496a959698afc8bf1495
sailgu <juyou3hui@gmail.com>
2022-06-18 17:54:40 +0800

    Update init.sh

--- Linux/init.sh
+++ Linux/init.sh
@@ -1,3 +1,9 @@
 apt-get update
 apt-get install unzip
 apt-get install zip
+apt-get install git
+pip3 install jedi
+
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
+
+curl -sL install-node.vercel.app/lts | bash
