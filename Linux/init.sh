493d1de330bfb2a84dd48295e03473f3d8bc83dd
sailgu <juyou3hui@gmail.com>
2022-06-19 10:18:21 +0800

    Update init.sh

--- Linux/init.sh
+++ Linux/init.sh
@@ -2,8 +2,15 @@ apt-get update
 apt-get install unzip
 apt-get install zip
 apt-get install git
-pip3 install jedi
 
-git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/jedi-vim
 
+#安装vim coc 补全插件
 curl -sL install-node.vercel.app/lts | bash
+mkdir -p ~/.vim/pack/coc/start
+cd ~/.vim/pack/coc/start
+git clone --branch release https://github.com/neoclide/coc.nvim.git --depth=1
+vim -c "helptags coc.nvim/doc/ | q"
+
+:CocInstall @yaegassy/coc-intelephense
+:CocInstall coc-tsserver coc-json coc-html coc-css
+:CocInstall coc-pyright
